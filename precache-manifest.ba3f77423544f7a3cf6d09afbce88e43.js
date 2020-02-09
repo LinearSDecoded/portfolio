4c599e9d6fa51fa942393925894f414513f4acee
Stanislav Olsson <ovosh90@gmail.com>
2020-02-09 14:06:57 +0100

    Updates

---
+++ precache-manifest.ba3f77423544f7a3cf6d09afbce88e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac5959c8116782c418f2199c7477d75",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af0355ff5981d1cb23eb",
+    "url": "/portfolio/static/css/main.15303154.chunk.css"
+  },
+  {
+    "revision": "e19a959546348951ebe7",
+    "url": "/portfolio/static/js/2.fac3e89c.chunk.js"
+  },
+  {
+    "revision": "7d4610b035b1afebd39e5e8fd5e70ce7",
+    "url": "/portfolio/static/js/2.fac3e89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0355ff5981d1cb23eb",
+    "url": "/portfolio/static/js/main.fc588852.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
